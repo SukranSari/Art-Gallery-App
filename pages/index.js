3b1bf534a9d00cb507bc0ee0209d0e7d3c609ef2
SukranSari <sukran_sari@hotmail.de>
2024-07-09 11:59:55 +0200

    Fetch all art pieces with SWR in pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
+// export default function HomePage() {
+//   return (
+//     <div>
+//       <h1>Hello from Next.js</h1>
+//     </div>
+//   );
+// }
+import useSWR from "swr";
+
 export default function HomePage() {
-  return (
-    <div>
-      <h1>Hello from Next.js</h1>
-    </div>
-  );
+  const fetcher = (...args) => fetch(...args).then((response) => response.json);
+
+  function Character() {
+    const { data } = useSWR("https://example-apis.vercel.app/api/art", fetcher);
+
+    // render data
+    return <div>Hello {data.artist}!</div>; // Hello Steve Johnson!
+  }
 }
