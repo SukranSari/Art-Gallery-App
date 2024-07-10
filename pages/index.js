a78f549140dadf0543f3ba9b77d439493fe45809
SukranSari <sukran_sari@hotmail.de>
2024-07-10 02:18:41 +0200

    Done: Display a Random_image with Informations like artist and title

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import useSWR from "swr";
 import ArtPieces from "@/components/ArtPieces";
+import Spotlight from "@/components/Spotlight";
+import styled from "styled-components";
 
 const fetcher = (...args) => fetch(...args).then((response) => response.json());
 const url = "https://example-apis.vercel.app/api/art";
@@ -12,10 +14,21 @@ export default function HomePage() {
   if (error) return <h1>Error, please try again!</h1>;
   if (isLoading) return <h1>Loading..</h1>;
 
+  /*Random-Image*/
+  const spotlightImage = pieces[Math.floor(Math.random() * pieces.length)];
+  console.log("spotlightImage", spotlightImage);
+  const artist = spotlightImage.artist;
+
   return (
     <>
+      <h4>Spotlight-Image</h4>
+      <Spotlight
+        spotlightImage={spotlightImage}
+        artist={spotlightImage.artist}
+      />
+
       <h1>Art Gallery</h1>
-      {<ArtPieces pieces={pieces} />}
+      <ArtPieces pieces={pieces} />
     </>
   );
 }
